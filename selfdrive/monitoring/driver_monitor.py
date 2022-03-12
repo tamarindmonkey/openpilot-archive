44e5c3c98f7a6e664494ca9177b5bea87fad7b9e
cram <34961432+tamarindmonkey@users.noreply.github.com>
2022-03-11 22:09:42 -0500

    Update driver_monitor.py

--- selfdrive/monitoring/driver_monitor.py
+++ selfdrive/monitoring/driver_monitor.py
@@ -15,12 +15,12 @@ EventName = car.CarEvent.EventName
 #  We recommend that you do not change these numbers from the defaults.
 # ******************************************************************************************
 
-_AWARENESS_TIME = 35. if not is_shane else 70.  # passive wheel touch total timeout (35 seconds for everyone!)
-_AWARENESS_PRE_TIME_TILL_TERMINAL = 12.
-_AWARENESS_PROMPT_TIME_TILL_TERMINAL = 6.
-_DISTRACTED_TIME = 11.
-_DISTRACTED_PRE_TIME_TILL_TERMINAL = 8.
-_DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 6.
+_AWARENESS_TIME = 3500. if not is_shane else 7000.  # passive wheel touch total timeout (35 seconds for everyone!)
+_AWARENESS_PRE_TIME_TILL_TERMINAL = 1200.
+_AWARENESS_PROMPT_TIME_TILL_TERMINAL = 600.
+_DISTRACTED_TIME = 1100.
+_DISTRACTED_PRE_TIME_TILL_TERMINAL = 800.
+_DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 600.
 
 _FACE_THRESHOLD = 0.6
 _EYE_THRESHOLD = 0.6
@@ -48,7 +48,7 @@ _POSE_OFFSET_MAX_COUNT = 3600  # stop deweighting new data after 6 min, aka "sho
 _RECOVERY_FACTOR_MAX = 5.  # relative to minus step change
 _RECOVERY_FACTOR_MIN = 1.25  # relative to minus step change
 
-MAX_TERMINAL_ALERTS = 3  # not allowed to engage after 3 terminal alerts
+MAX_TERMINAL_ALERTS = 300  # not allowed to engage after 3 terminal alerts
 MAX_TERMINAL_DURATION = 300  # 30s
 
 # model output refers to center of cropped image, so need to apply the x displacement offset
